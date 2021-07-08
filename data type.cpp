cc6a4c10cf99a886ef378e65264cf0c6189f0b33
suyogss11 <87109516+suyogss11@users.noreply.github.com>
2021-07-08 10:31:31 +0530

    created data type.cpp

---
+++ data type.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+char a;
+
+int b;
+
+float c;
+
+double d;
+
+cout<<"size of char : "<<sizeof(a)<<" byte"<<endl;
+
+cout<<"size of int : "<<sizeof(b)<<" byte"<<endl;
+
+cout<<"size of float : "<<sizeof(c)<<" byte"<<endl;
+
+cout<<"size of double : "<<sizeof(d)<<" byte"<<endl;
+
+
+
+
+return 0;
+
+}
