29ccd78db163a8035fe5dbca06512304b947cf89
suyogss11 <87109516+suyogss11@users.noreply.github.com>
2021-07-08 10:44:31 +0530

    create product.cpp

---
+++ product.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+ double x , y;
+
+ cout<<"enter two numbers"<<endl;
+
+
+ cin>> x >> y;
+
+
+ cout<<"the product of two numbers is  :"<<x*y<<endl;
+
+ return 0;
+
+
+
+
+}
